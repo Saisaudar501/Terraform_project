b54c6c97ba0e31b91773b277d6c94fcf69e1816d
Saisaudar501 <saisaudar@gmail.com>
2024-05-20 14:53:57 +0530

    output updated

--- Custom_Network/output.tf
+++ Custom_Network/output.tf
@@ -1,17 +1,17 @@
 #output block is used to print the required values
 
-output "aws_instance" {
+output "instance_ami" {
 value = aws_instance.pub_instance.ami  
 }
-output "aws_instance" {
-    value = aws_instance.pub_instance.public_ip 
+output "aws_instance_public_ip" {
+value = aws_instance.pub_instance.public_ip 
 }
-output "aws_instance" {
-    value = aws_instance.pub_instance.tags
+output "aws_instance_tag" {
+value = aws_instance.pub_instance.id
 }
-output "aws_vpc" {
-    value = aws_vpc.myvpc.tags
+output "aws_vpc_tag" {
+value = aws_vpc.myvpc.id
 }
-output "aws_subnet" {
-    value = aws_subnet.sub1.id
+output "aws_subnet_tag" {
+value = aws_subnet.sub1.id
 }
