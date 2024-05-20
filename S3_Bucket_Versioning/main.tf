ef71d5280d0500be78f5b4d17b05edafc7fa333a
Saisaudar501 <saisaudar@gmail.com>
2024-05-20 22:05:05 +0530

    updating the file

--- S3_Bucket_Versioning/main.tf
+++ S3_Bucket_Versioning/main.tf
@@ -4,6 +4,17 @@ resource "aws_s3_bucket" "rmbucket123" {
   bucket = "statefilestore"
 }
 
+#implementing locking for state file using dynamo DB 
+resource "aws_dynamodb_table" "DynamoStateFileLock" {
+  name = "statefilelock"
+  hash_key = "LockID"
+  read_capacity = 20
+  write_capacity = 20
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
 # creating a resource to enable versioning for bucket
 
 resource "aws_s3_bucket_versioning" "remote_bucket_versioning" {
